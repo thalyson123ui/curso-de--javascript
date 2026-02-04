90ab164b6c53b93535186413153ba52eb623b9a8
THALYSON JOSE DA ROSA SILVA <thalysonjosedarosasilva@gmail.com>
2026-02-04 08:46:15 -0300

    atualização

---
+++ aula07.js
@@ -0,0 +1,20 @@
+// operadores logicos em JavaScript
+console.log(15 > 10 && 5 < 3); // E lógico
+console.log(15 > 10 || 5 < 3); // OU lógico
+console.log(!(15 > 10));       // NÃO lógico
+
+// operadores de atribuição compostos
+let a = 20;        // atribuição simples
+a += 10;           // adição e atribuição
+a -= 5;            // subtração e atribuição
+a *= 2;            // multiplicação e atribuição
+a /= 5;            // divisão e atribuição
+
+console.log(a);
+
+// operadores de incremento e decremento
+let b = 8;
+console.log(b++); // pós-incremento
+console.log(++b); // pré-incremento
+console.log(b--); // pós-decremento
+console.log(--b); // pré-decremento
