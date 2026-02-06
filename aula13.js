e20487c7c774517f8dace1bc4ec09b982921ffbd
THALYSON JOSE DA ROSA SILVA <thalysonjosedarosasilva@gmail.com>
2026-02-06 09:32:05 -0300

    atualização

---
+++ aula13.js
@@ -0,0 +1,37 @@
+// Comando condicional IF e IF ELSE em Javascript
+var idade = 18;
+
+if (idade >= 18) {
+    console.log("Você é maior de idade.");
+} else {
+    console.log("Você é menor de idade.");
+}
+
+// Comando condicional SWITCH em Javascript
+var dia = 3;
+
+switch (dia) {
+    case 1:
+        console.log("Domingo");
+        break;
+    case 2:
+        console.log("Segunda-feira");
+        break;
+    case 3:
+        console.log("Terça-feira");
+        break;
+    case 4:
+        console.log("Quarta-feira");
+        break;
+    case 5:
+        console.log("Quinta-feira");
+        break;
+    case 6:
+        console.log("Sexta-feira");
+        break;
+    case 7:
+        console.log("Sábado");
+        break;
+    default:
+        console.log("Dia inválido");
+}
