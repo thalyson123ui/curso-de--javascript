5096249204c16859804426e6e7b1cd0e4a719d91
THALYSON JOSE DA ROSA SILVA <thalysonjosedarosasilva@gmail.com>
2026-02-09 09:35:58 -0300

    atualização

---
+++ aula19.js
@@ -0,0 +1,15 @@
+// break e continue
+
+for (let i = 0; i < 10; i++) {
+    if (i === 5) {
+        break
+    }
+    console.log(i)
+}
+
+for (let i = 0; i < 10; i++) {
+    if (i === 5) {
+        continue
+    }
+    console.log(i)
+}
