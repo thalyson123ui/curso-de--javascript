5465a684416b2ed99904e54b804c4a05e7dc331e
THALYSON JOSE DA ROSA SILVA <thalysonjosedarosasilva@gmail.com>
2026-02-09 09:29:03 -0300

    atualização

---
+++ aula18.js
@@ -0,0 +1,13 @@
+// while e do while
+
+let contador = 0
+
+while (contador < 10) {
+    console.log(contador)
+    contador++
+}
+
+do {
+    console.log(contador)
+    contador++
+} while (contador < 20)
