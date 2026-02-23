e2418aa7811ad039325df245d87164c84296ae39
THALYSON JOSE DA ROSA SILVA <thalysonjosedarosasilva@gmail.com>
2026-02-23 13:36:41 -0300

    removido

--- cursos.js
+++
@@ -1,4 +0,0 @@
-const cursos=["Javascript","HTML","CSS","Arduino","Raspberry","C++","Python","Java","C#"]
-const carros=["Polo","T-Cross","Golf","Onix","Cruze","Argo","Cronos"]
-
-export {cursos,carros}
